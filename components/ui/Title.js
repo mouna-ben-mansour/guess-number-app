57fd4afdf022fa9f69633b53df4fcba93101fc28
mouna-ben-mansour <benmansour.mouna2@gmail.com>
2023-10-11 15:08:21 +0100

    Platform API

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,4 +1,4 @@
-import {StyleSheet, Text} from "react-native";
+import {StyleSheet, Text, Platform} from "react-native";
 import Colors from "../../constants/colors";
 
 function Title({children}) {
@@ -13,7 +13,8 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 20,
         fontFamily: 'open-bold',
-        borderWidth: 2,
+       // borderWidth: Platform.OS === 'android' ? 2 : 0,
+        borderWidth: Platform.select({ ios: 0, android: 2}),
         borderColor: 'white',
         borderRadius: 15,
         padding: 15,
